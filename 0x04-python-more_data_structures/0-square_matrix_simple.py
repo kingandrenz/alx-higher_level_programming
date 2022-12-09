7b3c318418a88758e4d3033a98dd20f3d1ac40ca
kanu <kanuchibueze@gmail.com>
2022-12-08 19:20:35 -0800

    my commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return [[(row[i]**2) for row in matrix] for i in range(len(matrix))]
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
