6d60d403623a470fde512645dfc23db45e2cab57
kanu <kanuchibueze@gmail.com>
2023-02-16 19:18:25 -0800

    new commit

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- Create database 'hbtn_0d_2'
+-- If database already exists, script should not fail
+-- Create user 'user_0d_2'
+-- If user already exists, script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
